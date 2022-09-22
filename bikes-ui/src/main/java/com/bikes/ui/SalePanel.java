8148fce30cf3c10367a0f06c1ef7d63317492ff4
Iain MacLeish <imacleish@gmail.com>
2022-09-22 15:55:38 -0400

    implemented date filtering for sales

--- bikes-ui/src/main/java/com/bikes/ui/SalePanel.java
+++ bikes-ui/src/main/java/com/bikes/ui/SalePanel.java
@@ -1,24 +1,35 @@
 package com.bikes.ui;
 
 import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.RowFilter;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableRowSorter;
 
 import com.bikes.data.SaleData;
-import com.bikes.records.Customer;
 import com.bikes.records.FinalSale;
-import com.bikes.records.Sale;
 
 public class SalePanel extends JPanel {
 	
 	private SaleData source;
 	private List<FinalSale> data;
+	
+	private JTextField afterFilter;
+	private JTextField beforeFilter;
+	private TableRowSorter<ProductModel> sorter;
+	
+	
 
 	public SalePanel(SaleData sdata) {
 		this.source = sdata;
@@ -27,9 +38,51 @@ public class SalePanel extends JPanel {
 		this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		
+		// Top label with filter date pickers
+		JPanel filterPanel = new JPanel();
+		filterPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		filterPanel.setLayout(new BoxLayout(filterPanel, BoxLayout.LINE_AXIS));
+		this.add(filterPanel);
+		filterPanel.add(new JLabel("Filter by date (format YYYY-MM-DD) from "));
+		afterFilter = new JTextField("2022-1-1");
+		filterPanel.add(afterFilter);
+		filterPanel.add(new JLabel(" to "));
+		beforeFilter = new JTextField("2022-7-1");
+		filterPanel.add(beforeFilter);
+		JButton filterButton = new JButton("Filter");
+		filterButton.addActionListener(event -> sorter.sort());
+		filterPanel.add(filterButton);
+		
+		
+		// Table
+		ProductModel model = new ProductModel();
+		sorter = new TableRowSorter<>(model);
+		sorter.setRowFilter(new DateFilter());
 		JTable table = new JTable(new ProductModel());
+		table.setRowSorter(sorter);
 		JScrollPane scroll = new JScrollPane(table);
 		this.add(scroll);
+	}
+
+
+
+
+	private class DateFilter extends RowFilter<ProductModel, Integer> {
+		@Override
+		public boolean include(Entry<? extends ProductModel, ? extends Integer> entry) {
+			try {
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-d");
+				LocalDate afterDate = LocalDate.parse(afterFilter.getText(), formatter);
+				LocalDate beforeDate = LocalDate.parse(beforeFilter.getText(), formatter);
+				FinalSale row = data.get(entry.getIdentifier());
+				return row.date().isAfter(afterDate) &&
+					   row.date().isBefore(beforeDate);
+			} catch(Exception e) {
+				System.err.println("Bad time string");
+				return true;
+			}
+		}
+
 	}
 	
 	private class ProductModel extends AbstractTableModel {
